13eaa0cea6fca71d3816126ef5901c904c37fc50
mischlue <55716426+mischlue@users.noreply.github.com>
2020-05-20 11:03:30 -0400

    Update SMBSpike2.py

--- SMBSpike2.py
+++ SMBSpike2.py
@@ -8,8 +8,7 @@ for i in range(100):
         ip = "208.15.1.%d" % (i)
         print ip
         os.system("nc -vv -w1 " +ip + " 445") 
-        print "scanning:", ip
-        time.sleep(4)
+        time.sleep(2)
 
 print "scanning complete!"        
 
