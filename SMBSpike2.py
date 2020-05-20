a24995a2eaa2b9d0c03f3d1837f0fd62d720f971
mischlue <55716426+mischlue@users.noreply.github.com>
2020-05-20 10:17:48 -0400

    Update SMBSpike2.py

--- SMBSpike2.py
+++ SMBSpike2.py
@@ -5,9 +5,9 @@ import time
 
 for i in range(100):
     
-        ip = "1.1.1.%d" % (i)
+        ip = "208.15.1.%d" % (i)
         print ip
-        os.system("nc -n -z -v " + ip + "445") 
+        os.system("nc -n -z -v " +ip + " 445") 
         print "scanning:", ip
         time.sleep(4)
 
