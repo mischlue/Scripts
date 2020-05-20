5018bff4450ba0c4d493610a07aeaccb851ab552
mischlue <55716426+mischlue@users.noreply.github.com>
2020-05-20 10:02:42 -0400

    Add files via upload

---
+++ SMBSpike2.py
@@ -0,0 +1,18 @@
+import os
+import time 
+
+
+
+for i in range(100):
+    
+        ip = "1.1.1.%d" % (i)
+        print ip
+        os.system("nc -n -z -v " + ip + "445") 
+        print "scanning:", ip
+        time.sleep(4)
+
+print "scanning complete!"        
+
+
+
+
